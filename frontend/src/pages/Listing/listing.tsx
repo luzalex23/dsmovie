af8d3cb490dce7be70357cd5121d1692308eacf6
luzalex23 <alexluzendreyravi@gmail.com>
2022-03-11 18:08:02 -0300

    First request

--- frontend/src/pages/Listing/listing.tsx
+++ frontend/src/pages/Listing/listing.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard/moviecard";
 import Pagination from "components/Pagination/pagnation";
+import { BASE_URL } from "ultils/requests";
 
 function Listing(){
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response =>{
+        console.log(response.data);
+    });
     return(
         <>
             <Pagination />
