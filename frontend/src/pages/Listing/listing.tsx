35316f2d71f7090c55669527830003d1be48cac4
luzalex23 <alexluzendreyravi@gmail.com>
2022-03-12 14:44:10 -0300

    useState, useEffect

--- frontend/src/pages/Listing/listing.tsx
+++ frontend/src/pages/Listing/listing.tsx
@@ -1,16 +1,26 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard/moviecard";
 import Pagination from "components/Pagination/pagnation";
+import { useState, useEffect } from "react";
+import { MoviePage } from "types/Movies";
 import { BASE_URL } from "ultils/requests";
 
-function Listing(){
+function Listing(){ 
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response =>{
-        console.log(response.data);
-    });
+    const[pageNumber,setPageNumber] = useState(0);
+
+    useEffect(()=>{
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response =>{
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
+        });
+    },[]);
+   
     return(
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
